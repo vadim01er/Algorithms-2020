9be8b52313f4f8854720111fe6a7a0a9637838a5
Vadim Ershov <vadim.ershov.01@mail.ru>
2020-11-29 02:30:55 +0500

    realized lesson 5

--- src/lesson5/OpenAddressingSet.java
+++ src/lesson5/OpenAddressingSet.java
@@ -1,11 +1,8 @@
 package lesson5;
 
-import kotlin.NotImplementedError;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.AbstractSet;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
 
 public class OpenAddressingSet<T> extends AbstractSet<T> {
 
@@ -16,6 +13,10 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
     private final Object[] storage;
 
     private int size = 0;
+
+    private enum State {
+        REMOVED
+    }
 
     private int startingIndex(Object element) {
         return element.hashCode() & (0x7FFFFFFF >> (31 - bits));
@@ -54,10 +55,10 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
 
     /**
      * Добавление элемента в таблицу.
-     *
+     * <p>
      * Не делает ничего и возвращает false, если такой же элемент уже есть в таблице.
      * В противном случае вставляет элемент в таблицу и возвращает true.
-     *
+     * <p>
      * Бросает исключение (IllegalStateException) в случае переполнения таблицы.
      * Обычно Set не предполагает ограничения на размер и подобных контрактов,
      * но в данном случае это было введено для упрощения кода.
@@ -67,7 +68,7 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
         int startingIndex = startingIndex(t);
         int index = startingIndex;
         Object current = storage[index];
-        while (current != null) {
+        while (current != null && current != State.REMOVED) {
             if (current.equals(t)) {
                 return false;
             }
@@ -84,34 +85,85 @@ public class OpenAddressingSet<T> extends AbstractSet<T> {
 
     /**
      * Удаление элемента из таблицы
-     *
+     * <p>
      * Если элемент есть в таблица, функция удаляет его из дерева и возвращает true.
      * В ином случае функция оставляет множество нетронутым и возвращает false.
      * Высота дерева не должна увеличиться в результате удаления.
-     *
+     * <p>
      * Спецификация: {@link Set#remove(Object)} (Ctrl+Click по remove)
-     *
+     * <p>
      * Средняя
      */
+    //Асимптотика: O(capacity)
     @Override
     public boolean remove(Object o) {
-        return super.remove(o);
+        int startingIndex = startingIndex(o);
+        int index = startingIndex;
+        T toRemove = (T) storage[index];
+        while (toRemove != null) {
+            if (toRemove.equals(o)) {
+                storage[index] = State.REMOVED;
+                size--;
+                return true;
+            }
+            index = (index + 1) % capacity;
+            if (index == startingIndex) {
+                return false;
+            }
+            toRemove = (T) storage[index];
+        }
+        return false;
     }
 
     /**
      * Создание итератора для обхода таблицы
-     *
+     * <p>
      * Не забываем, что итератор должен поддерживать функции next(), hasNext(),
      * и опционально функцию remove()
-     *
+     * <p>
      * Спецификация: {@link Iterator} (Ctrl+Click по Iterator)
-     *
+     * <p>
      * Средняя (сложная, если поддержан и remove тоже)
      */
     @NotNull
     @Override
     public Iterator<T> iterator() {
-        // TODO
-        throw new NotImplementedError();
+        return new OpenAddressingSetIterator();
+    }
+
+    public class OpenAddressingSetIterator implements Iterator<T> {
+
+        T last = null;
+        int index = 0;
+        int count = 0;
+
+        @Override
+        public boolean hasNext() {
+            return count < size;
+        }
+
+        //Асимптотика: O(capacity)
+        @Override
+        public T next() {
+            if (!hasNext())
+                throw new IllegalStateException();
+            while (storage[index] == null || storage[index] == State.REMOVED) {
+                index++;
+            }
+            last = (T) storage[index];
+            index++;
+            count++;
+            return last;
+        }
+
+        @Override
+        public void remove() {
+            if (last == null)
+                throw new IllegalStateException();
+            storage[--index] = State.REMOVED;
+            last = null;
+            size--;
+            count--;
+        }
     }
 }
